129698660844ffc2533bad5ee2c5bb635bc6b4f1
aladin2920 <alefpacheco@gmail.com>
2023-09-19 14:00:32 -0600

    version 1.0.0

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Resourse group name"
+}
+
+variable "location" {
+  type        = string
+  description = "Location"
+}
+
+variable "name" {
+  type        = string
+  description = "Name"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment"
+}
